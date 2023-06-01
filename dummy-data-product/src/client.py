7e66cf9daec1f3c946cab0a61b5ee6dcec2f6c72
ezhil6999 <ezhil6999@gmail.com>
2023-06-01 15:13:05 +0530

    toiyo.ai1

--- dummy-data-product/src/client.py
+++ dummy-data-product/src/client.py
@@ -1,54 +1,33 @@
-import dotenv
-import logging
 
 from datetime import datetime
+from dependencies.scraping.scraper import Scrapper
+from dependencies.cleaning.cleaning import clean_data
+from dependencies.geocoding.geocoder import geocode_csv
+from dependencies.standardization.standardizer import *
 
-# Importing scraping and data processing modules
-# from dependencies.scraping.<file_name> import <class_name>
-# from dependencies.scraping.<file_name> import <class_name>
-# from dependencies.cleaning.<file_name> import <class_name>
-# from dependencies.geocoding.<file_name> import <class_name>
-# from dependencies.standardization.<file_name> import <class_name>
-
-dotenv.load_dotenv(".env")
-logging.basicConfig(level=logging.INFO)
-
-
-# In each step create an object of the class, initialize the class with 
-# required configuration and call the run method 
 def step_1():
-    logging.info("Scraped Metadata")
+    filename = input("Enter csv file name : ")
+    writer, file = open_file(filename)
+    Scrapper(writer)
+    closefile(file)
 
 
 def step_2():
-    logging.info("Scraped Main Data")
+    clean_data()
 
 
 def step_3():
-    logging.info("Cleaned Main Data")
+    geocode_csv()
+
+class Main:
+    filename = input("Enter csv file name : ")
+    writer,file=open_file(filename)
+    Scrapper(writer)
+    closefile(file)
+    t=True
+    while(t):
+        print("1-->Scrapping Meta Data\n2-->Cleaned Main Data\n3-->Geocoded Cleaned Data")
+        stepnum = int(input("Enter step to go :"))
+        eval(f"step_{stepnum}()")
 
 
-def step_4():
-    logging.info("Geocoded Cleaned Data")
-
-
-def step_5():
-    logging.info("Standardized Geocoded Data")
-
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--step", help="step to be choosen for execution")
-
-    args = parser.parse_args()
-
-    eval(f"step_{args.step}()")
-
-    logging.info(
-        {
-            "last_executed": str(datetime.now()),
-            "status": "Pipeline executed successfully",
-        }
-    )
